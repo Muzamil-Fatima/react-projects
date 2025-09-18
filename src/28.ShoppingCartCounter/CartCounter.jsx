b55640a177a1ef88deec93c0ebb75bbbf935e7c3
Muzamil <muzamilzubair987654@gmail.com>
2025-09-18 21:08:44 +0500

    Add file

--- src/28.ShoppingCartCounter/CartCounter.jsx
+++ src/28.ShoppingCartCounter/CartCounter.jsx
@@ -1,7 +1,11 @@
-export default function CartCounter(){
-    return(
-        <>
-        
-        </>
-    )
+import WithLibrary from "./WithLibrary";
+import WithOutLibrary from "./WithOutLibrary";
+
+export default function CartCounter() {
+  return (
+    <>
+      <WithLibrary />
+      <WithOutLibrary />
+    </>
+  );
 }
--- src/31.LiveDigitalClock/Clock.jsx
+++
@@ -1,7 +0,0 @@
-export default function Clock(){
-    return(
-        <>
-        
-        </>
-    )
-}
