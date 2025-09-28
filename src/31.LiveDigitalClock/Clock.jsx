8c3a324a4feb32015afc84074446d8ab0b31dc2c
Muzamil <muzamilzubair987654@gmail.com>
2025-09-28 19:00:28 +0500

    remove error

--- src/31.LiveDigitalClock/Clock.jsx
+++ src/31.LiveDigitalClock/Clock.jsx
@@ -68,28 +68,32 @@ export default function Clock() {
   };
   const formatTimer = () => {
     const minutes = Math.floor(timer / 60);
-    const seconds = time % 60;
+    const seconds = timer % 60;
     return `${minutes.toString().padStart(2, "0")}: ${seconds
       .toString()
       .padStart(2, "0")}`;
   };
   return (
     <div
-      className={`flex flex-col justify-center items-center min-h-screen rounded-4xl  ${
+      className={`flex flex-col justify-center items-center min-h-screen rounded-4xl px-4 sm:px-6 md:px-12 lg:px-20 ${
         mode ? "bg-white text-black" : "bg-gray-800 text-white"
       }`}
     >
-      <h2 className="text-2xl font-extrabold m-8">⏰ React Clock</h2>
+      <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-extrabold m-4">
+        ⏰ React Clock
+      </h2>
       <div className="space-y-4">
-        <p className="text-2xl text-amber-600 font-bold flex">
+        <p className="text-lg sm:text-xl md:text-2xl text-amber-600 font-bold flex justify-center">
           {formattedTime}
         </p>
-        <p className="text-2xl ">{time.toDateString()}</p>
-        <div className="flex">
+        <p className="text-base sm:text-lg md:text-xl ">
+          {time.toDateString()}
+        </p>
+        <div className="flex flex-col sm:flex-row gap-2 mt-4">
           <p className="text-red-600 font-bold">Switch to</p>
           <button
             onClick={() => setIs12Hour(!is12Hour)}
-            className="bg-amber-300 px-4 py-1.5 ml-2 rounded-2xl hover:bg-blue-500"
+            className="bg-amber-300 px-3 py-2 rounded-2xl hover:bg-blue-500"
           >
             {is12Hour ? "24-Hour" : "12-Hour"}
           </button>
@@ -101,46 +105,46 @@ export default function Clock() {
           </button>
         </div>
       </div>
-      <div className="">
+      <div className="flex flex-col sm:flex-row gap-2 justify-center mt-4">
         <p className="text-red-600 font-bold m-4">
           Stopwatch: {formatStopWatch()}
         </p>
         <button
           onClick={handleStart}
-          className="bg-amber-300 px-4 py-1.5 ml-2 rounded-2xl hover:bg-blue-500"
+          className="bg-amber-300 px-3 py-2 rounded-lg hover:bg-blue-500"
         >
           Start
         </button>
         <button
           onClick={handleStop}
-          className="bg-amber-300 px-4 py-1.5 ml-2 rounded-2xl hover:bg-blue-500"
+          className="bg-amber-300 px-3 py-2 rounded-lg hover:bg-blue-500"
         >
           Stop
         </button>
         <button
           onClick={handleReset}
-          className="bg-amber-300 px-4 py-1.5 ml-2 rounded-2xl hover:bg-blue-500"
+          className="bg-amber-300 px-3 py-2 rounded-2xl hover:bg-blue-500"
         >
           Reset
         </button>
       </div>
-      <div>
+      <div className="flex flex-col sm:flex-row gap-2 justify-center mt-4">
         <p className="text-red-600 font-bold m-4">Timer: {formatTimer()}</p>
         <button
           onClick={handleTimerStart}
-          className="bg-amber-300 px-4 py-1.5 ml-2 rounded-2xl hover:bg-blue-500"
+          className="bg-amber-300 px-3 py-2  rounded-2xl hover:bg-blue-500"
         >
           Start
         </button>
         <button
           onClick={handleTimerPause}
-          className="bg-amber-300 px-4 py-1.5 ml-2 rounded-2xl hover:bg-blue-500"
+          className="bg-amber-300 px-3 py-2  rounded-2xl hover:bg-blue-500"
         >
           Pause
         </button>
         <button
           onClick={handleTimeReset}
-          className="bg-amber-300 px-4 py-1.5 ml-2 rounded-2xl hover:bg-blue-500"
+          className="bg-amber-300 px-3 py-2 rounded-2xl hover:bg-blue-500"
         >
           Reset
         </button>
