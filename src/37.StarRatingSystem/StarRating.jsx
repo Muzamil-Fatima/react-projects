cc00d3de79c2deb22866266409da812ab6216383
Muzamil <muzamilzubair987654@gmail.com>
2025-10-01 17:29:07 +0500

    Add file

--- src/37.StarRatingSystem/StarRating.jsx
+++ src/37.StarRatingSystem/StarRating.jsx
@@ -1,30 +1,29 @@
 import { useState } from "react";
 export default function StarRating() {
-  const [rating, setRating] = useState(0);
-  const [hover, setHover] = useState(0);
+  const [rating, setRating] = useState();
+  const [hover, setHover] = useState();
+  const star = [1, 2, 3, 4, 5];
   return (
-    <div className="min-h-screen flex flex-col item-center justify-center bg-gary-900 text-white">
-      <h1 className="text-2xl font-bold mb-4"> ⭐ Star Rating</h1>
-      <div className="flex space-x-2">
-        {[1, 2, 3, 4, 5].map((star) => {
+    <>
+      <h1 className="mb-6 font-bold text-8xl"> ⭐ Star Rating </h1>
+      <div>
+        {star.map((stars) => (
           <span
-            key={star}
-            className={`cursor-pointer text-4xl transition-colors ${
-              (hover || rating) >= star ? "text-yellow" : "text-gray-500"
-            }`}
-            onClick={setRating(star)}
-            onMouseEnter={() => setHover(star)}
+            key={stars}
+            onClick={() => setRating(stars)}
+            onMouseEnter={() => setHover(stars)}
             onMouseLeave={() => setHover(0)}
+            className="cursor-pointer text-4xl"
           >
-            ⭐
-          </span>;
-        })}
+            {stars <= (hover || rating) ? "⭐" : "☆"}
+          </span>
+        ))}
       </div>
       {rating > 0 && (
         <p>
-          You Rated: <span className="font-bold">{rating}</span>
+          You Rated : <span className="font-bold">{rating}</span>
         </p>
       )}
-    </div>
+    </>
   );
 }
